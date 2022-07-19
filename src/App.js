92abdf745a9b443b8b54a011397b95e331f56068
Ferdinand McDaniel <imcdanielamano@gmail.com>
2022-07-19 15:41:17 +0800

    initial setup + tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">Some Title</h1>
+      <p id="description">some description</p>
+      <form id="survey-form" aria-label='form'>
+        <label for="name" id="name-label" aria-label='name label'>Name</label>
+        <input id="name"  required/>
+        <label for="email" id="email-label" aria-label='email label'>Email</label>
+        <input id="email" type="email"  required/>
+        <label for="number" id="number-label" aria-label='number label'>Age</label>
+        <input id="number" type="number" max="65" min="10"/>
+        <select>
+          <option value="">(select one)</option>
+          <option value="1">freeCodeCamp News</option>
+        </select>
+        <fieldset>
+          <label><input type="radio" name="account-type" class="inline" /> Personal Account</label>
+          <label><input type="radio" name="account-type" class="inline" /> Business Account</label>
+          <label>
+            <input type="checkbox" name="terms" class="inline" required /> I accept the <a href="https://www.freecodecamp.org/news/terms-of-service/">terms and conditions</a>
+            <input type="checkbox" name="terms" class="inline" required /> I accept the <a href="https://www.freecodecamp.org/news/terms-of-service/">terms and conditions</a>
+          </label>
+        </fieldset>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,107 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { render, screen } from "@testing-library/react"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Survey Page', () => {
+  describe('Layout', () => {
+      it("has header", () => {
+          render(<App />);
+          const header = screen.queryByRole("heading");
+          expect(header).toBeInTheDocument();
+          expect(header.id).toEqual("title");
+          expect(header).not.toBeEmptyDOMElement();
+      })
+      it('has description', () => {
+          render(<App />);
+          const element = document.getElementById("description");
+          expect(element).not.toBeNull();
+      });
+      it('has form', () => {
+          render(<App />);
+          const form = screen.queryByRole("form");
+          expect(form).toBeInTheDocument();
+          expect(form.id).toEqual("survey-form");
+      });
+      it('has name input', () => {
+          render(<App />);
+          const input = screen.getByLabelText("Name");
+          expect(input).toBeInTheDocument();
+          expect(input.type).toBe("text");
+          expect(input.id).toEqual("name");
+          expect(input.required).not.toBeFalsy();
+          expect(input.parentNode.id).toEqual("survey-form");
+      });
+      it('has name label', () => {
+          render(<App />);
+          const label = screen.getByText("Name"); 
+          expect(label.id).toEqual("name-label");
+          expect(label).toBeInTheDocument();
+          expect(label).not.toBeEmptyDOMElement();
+          expect(label.parentNode.id).toEqual("survey-form");
+      });
+      it('has email input', () => {
+          render(<App />);
+          const input = screen.getByLabelText("Email");
+          expect(input).toBeInTheDocument();
+          expect(input.type).toBe("email");
+          expect(input.id).toEqual("email");
+          expect(input.required).not.toBeFalsy();
+          expect(input.parentNode.id).toEqual("survey-form");
+      });
+      it('has email label', () => {
+          render(<App />);
+          const label = screen.getByText("Email"); 
+          expect(label.id).toEqual("email-label");
+          expect(label).toBeInTheDocument();
+          expect(label).not.toBeEmptyDOMElement();
+          expect(label.parentNode.id).toEqual("survey-form");
+      });
+      it('has age input', () => {
+          render(<App />);
+          const input = screen.getByLabelText("Age");
+          expect(input).toBeInTheDocument();
+          expect(input.type).toBe("number");
+          expect(input.id).toEqual("number");
+          expect(input.min).not.toBeFalsy();
+          expect(input.max).not.toBeFalsy();
+          expect(input.parentNode.id).toEqual("survey-form");
+      });
+      it('has age label', () => {
+          render(<App />);
+          const label = screen.getByText("Age"); 
+          expect(label.id).toEqual("number-label");
+          expect(label).toBeInTheDocument();
+          expect(label).not.toBeEmptyDOMElement();
+          expect(label.parentNode.id).toEqual("survey-form");
+      });
+      it('has dropdown', () => {
+          render(<App />);
+          const dropdown = screen.getByRole("combobox");
+          const container = document.getElementById("survey-form");
+          expect(dropdown).toBeInTheDocument();
+          expect(dropdown.childNodes.length).toBeGreaterThan(1);
+          expect(container.contains(dropdown)).toBeTruthy();
+      });
+      it('should have radio buttons', () => {
+          render(<App />);
+          const radioButtons = screen.queryAllByRole('radio');
+          const container = document.getElementById("survey-form");
+          expect(radioButtons.length).toBeGreaterThan(1);
+          radioButtons.forEach(element => {
+            expect(element.value).not.toBeFalsy();
+            expect(element.name).not.toBeFalsy();
+            expect(container.contains(element)).toBeTruthy();
+          });
+      });
+      it('should have checkboxes', () => {
+          render(<App />);
+          const radioButtons = screen.queryAllByRole('checkbox');
+          const container = document.getElementById("survey-form");
+          expect(radioButtons.length).toBeGreaterThan(1);
+          radioButtons.forEach(element => {
+            expect(element.value).not.toBeFalsy();
+            expect(element.name).not.toBeFalsy();
+            expect(container.contains(element)).toBeTruthy();
+          });
+      });
+  });
 });
